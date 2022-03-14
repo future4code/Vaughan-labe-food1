1e2ccfeef0ecee8d466a6ef20e6f7997ffdd70e1
Ronald <ronald.santiago46@gmail.com>
2022-03-14 17:11:16 -0300

    Filtros

--- labe-food/src/pages/AddAddress/AddAddress.js
+++ labe-food/src/pages/AddAddress/AddAddress.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Button, TextField, Typography } from "@mui/material";
 import { BasicCard, StyledForm, StyledCabecalho } from "./styled-addaddress";
 import ArrowBackIosRoundedIcon from "@mui/icons-material/ArrowBackIosRounded";
-
+import useForm from "../../hooks/useform";
 const AddAddress = () => {
-  const { form, onChange, clear } = useform({
+  const { form, onChange, clear } = useForm({
     street: "",
     number: "",
     neighbourhood: "",
