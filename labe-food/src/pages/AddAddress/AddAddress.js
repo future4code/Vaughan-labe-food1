943855c3ab27ba84f07e3aa6c6e99d1ef3b87fec
Gabriel Dias Lopes Martins <gdlmartins@Gabriels-MBP.gateway>
2022-03-15 00:50:15 +0800

    mui from address page

--- labe-food/src/pages/AddAddress/AddAddress.js
+++ labe-food/src/pages/AddAddress/AddAddress.js
@@ -1,13 +1,66 @@
 import React from 'react';
-import {TextField} from "@mui/material";
+import {Button, TextField, Typography} from "@mui/material";
+import {BasicCard , StyledForm} from './styled-addaddress';
 
 
 const AddAddress = () => {
     return (
-        <div>
-            <TextField/>
-            
-        </div>
+        
+
+        <BasicCard>
+            <hr/>
+        <Typography align="center">Meu endereço</Typography>
+        <StyledForm> 
+          <TextField 
+    
+          label="Logradouro"
+          required
+          variant="outlined"
+          placeholder="Rua / Av"
+          margin="dense"   
+          />
+          <TextField 
+          label="Número"
+          required
+          variant="outlined"
+          placeholder="Número"
+          margin="dense"   
+          />
+          <TextField 
+          label="Complemento"
+          placeholder="Apto/Bloco"
+          margin="dense"   
+          />
+          <TextField 
+          label="Bairro"
+          required
+          variant="outlined"
+          placeholder="Bairro"
+          margin="dense"   
+          />
+      
+          <TextField 
+          label="Cidade"
+          required
+          variant="outlined"
+          placeholder="Cidade"
+          margin="dense"   
+          />
+          <TextField 
+          label="Estado"
+          required
+          variant="outlined"
+          placeholder="Estado"
+          margin="dense"   
+          />
+          <Button
+          type="form"
+          color="secondary"
+          variant="contained"
+          size="small"
+          >Salvar</Button>
+          </StyledForm>
+        </BasicCard>
     )
 };
 
---
+++ labe-food/src/pages/AddAddress/styled-addaddress.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import styled from 'styled-components'
+
+
+
+export const StyledForm = styled.div`
+margin=10px;
+display: flex;
+flex-direction: column;
+`
+
+
+export  function BasicCard(props) {
+  return (
+    <Card 
+
+    style={{display: "flex", flexDirection: "column" }}
+ >
+     {props.children}
+    </Card>
+  );
+}
+
