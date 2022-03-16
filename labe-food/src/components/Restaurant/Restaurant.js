6b792bf7f65b7ee62fb22744d186ae7d59d6d6af
Gabriel Dias Lopes Martins <gdlmartins@Gabriels-MBP.gateway>
2022-03-16 14:03:28 +0800

    adding headers to all pages

--- labe-food/src/components/Restaurant/Restaurant.js
+++ labe-food/src/components/Restaurant/Restaurant.js
@@ -1,89 +1,126 @@
-import { Button, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { baseURL } from "../../constants/baseurl";
 import useRequestData from "../../hooks/useRequestData";
+import Header from "../Header/Header";
 import {
-    ButtonAdd, ButtonDiv, CardProducts, CardRestaurant, MainDiv, ProductImage,
-    ProductText, ShippingAndTime, TimeStyled, TypographyStyled
+  ButtonAdd,
+  ButtonDiv,
+  CardProducts,
+  CardRestaurant,
+  MainDiv,
+  ProductImage,
+  ProductText,
+  ShippingAndTime,
+  TimeStyled,
+  TypographyStyled,
 } from "./styled-restaurant";
 
 const Restaurant = () => {
-    const params = useParams();
-    const [restaurantDetails] = useRequestData([], `${baseURL}/restaurants/${params.id}`);
+  const params = useParams();
+  const [restaurantDetails] = useRequestData(
+    [],
+    `${baseURL}/restaurants/${params.id}`
+  );
 
-    const cardRestaurant = restaurantDetails.restaurant
+  const cardRestaurant = restaurantDetails.restaurant;
 
-    // Tentando separar os produtos por categoria!!
+  // Tentando separar os produtos por categoria!!
 
-    // useEffect(() => {
-    //     addingCategory()
-    // }, [restaurantDetails])
+  // useEffect(() => {
+  //     addingCategory()
+  // }, [restaurantDetails])
 
-    // const addingCategory = () => {
-    //     if (cardRestaurant) {
-    //         let category = []
-    //         let p = 0
-    //         for (let i = 0; i < cardRestaurant.products.length; i++) {
-    //             console.log(cardRestaurant.products[i].category)
-    //             if (cardRestaurant.products[i].category !== category.name) {
-    //                 category.push({ name: cardRestaurant.products[i].category, products: [cardRestaurant.products[i]] })
-    //             }
-    //         }
-    //         console.log(category)
-    //     }
-    // }
+  // const addingCategory = () => {
+  //     if (cardRestaurant) {
+  //         let category = []
+  //         let p = 0
+  //         for (let i = 0; i < cardRestaurant.products.length; i++) {
+  //             console.log(cardRestaurant.products[i].category)
+  //             if (cardRestaurant.products[i].category !== category.name) {
+  //                 category.push({ name: cardRestaurant.products[i].category, products: [cardRestaurant.products[i]] })
+  //             }
+  //         }
+  //         console.log(category)
+  //     }
+  // }
 
-    const renderProducts = cardRestaurant && cardRestaurant.products.map((product) => {
-        return (
-            <CardProducts key={product.id} variant="outlined">
-                <ProductImage
-                    component="img"
-                    height="150"
-                    image={product.photoUrl}
-                    alt="Foto do Restaurante"
-                />
-                <ProductText>
-                    <TypographyStyled variant="body" color="primary">{product.name}</TypographyStyled>
-                    <TypographyStyled variant="body2" color="secondary">{product.description}</TypographyStyled>
-                    <TypographyStyled variant="body">R${product.price}0</TypographyStyled>
-                </ProductText>
-                <ButtonDiv>
-                    <ButtonAdd variant="outlined" color="inherit">Adicionar</ButtonAdd>
-                </ButtonDiv>
-            </CardProducts>
-        )
+  const renderProducts =
+    cardRestaurant &&
+    cardRestaurant.products.map((product) => {
+      return (
+        <CardProducts key={product.id} variant='outlined'>
+          <ProductImage
+            component='img'
+            height='150'
+            image={product.photoUrl}
+            alt='Foto do Restaurante'
+          />
+          <ProductText>
+            <TypographyStyled variant='body' color='primary'>
+              {product.name}
+            </TypographyStyled>
+            <TypographyStyled variant='body2' color='secondary'>
+              {product.description}
+            </TypographyStyled>
+            <TypographyStyled variant='body'>
+              R${product.price}0
+            </TypographyStyled>
+          </ProductText>
+          <ButtonDiv>
+            <ButtonAdd variant='outlined' color='inherit'>
+              Adicionar
+            </ButtonAdd>
+          </ButtonDiv>
+        </CardProducts>
+      );
     });
 
-    return (
-        <MainDiv>
-            <h2>Restaurante</h2>
-            {cardRestaurant &&
+  return (
+    <>
+      <Header title='Restaurante' />
+      <MainDiv>
+        {cardRestaurant && (
+          <CardRestaurant>
+            <CardMedia
+              component='img'
+              height='150'
+              image={cardRestaurant.logoUrl}
+              alt='Foto do Restaurante'
+            />
+            <CardContent>
+              <Typography variant='body' component='div' color='primary'>
+                {cardRestaurant.name}
+              </Typography>
+              <Typography variant='body2' color='secondary'>
+                {cardRestaurant.category}
+              </Typography>
+              <ShippingAndTime>
+                <TimeStyled variant='body2' color='secondary'>
+                  {cardRestaurant.deliveryTime} min
+                </TimeStyled>
+                <Typography variant='body2' color='secondary'>
+                  Frete R${cardRestaurant.shipping},00
+                </Typography>
+              </ShippingAndTime>
 
-                <CardRestaurant>
-                    <CardMedia
-                        component="img"
-                        height="150"
-                        image={cardRestaurant.logoUrl}
-                        alt="Foto do Restaurante"
-                    />
-                    <CardContent>
-                        <Typography variant="body" component="div" color="primary">
-                            {cardRestaurant.name}
-                        </Typography>
-                        <Typography variant="body2" color="secondary">{cardRestaurant.category}</Typography>
-                        <ShippingAndTime>
-                            <TimeStyled variant="body2" color="secondary">{cardRestaurant.deliveryTime} min</TimeStyled>
-                            <Typography variant="body2" color="secondary">Frete R${cardRestaurant.shipping},00</Typography>
-                        </ShippingAndTime>
-
-                        <Typography variant="body2" color="secondary">{cardRestaurant.address}</Typography>
-                    </CardContent>
-                </CardRestaurant>
-            }
-            {cardRestaurant && renderProducts}
-        </MainDiv>
-    )
+              <Typography variant='body2' color='secondary'>
+                {cardRestaurant.address}
+              </Typography>
+            </CardContent>
+          </CardRestaurant>
+        )}
+        {cardRestaurant && renderProducts}
+      </MainDiv>
+    </>
+  );
 };
 
 export default Restaurant;
--- labe-food/src/pages/Profile/Profile.js
+++ labe-food/src/pages/Profile/Profile.js
@@ -1,8 +1,16 @@
+import Header from "../../components/Header/Header";
+
+
 const Profile = () => {
     return (
+        <>
+        <Header 
+        title="Meu Perfil" 
+        goBack={true} />
         <div>
             Profile
         </div>
+        </>
     )
 };
 
