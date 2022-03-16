6df070b6639fb80ede82b03cfba5b5f337c0ed3c
Ronald <ronald.santiago46@gmail.com>
2022-03-16 07:51:15 -0300

    Página de Perfil

--- labe-food/src/pages/Profile/Profile.js
+++ labe-food/src/pages/Profile/Profile.js
@@ -1,9 +1,77 @@
-const Profile = () => {
-    return (
-        <div>
-            Profile
-        </div>
-    )
-};
+import React, { useEffect, useState } from "react";
+import { baseURL } from "../../constants/baseurl";
+import { useNavigate } from "react-router";
+import { createRenderer } from "react-dom/test-utils";
+import { goToAddress, goToEditProfile } from "../../routes/coordinator";
+import axios from "axios";
+import useRequestData from "../../hooks/useRequestData"
+import { TrendingUpOutlined } from "@mui/icons-material";
+import { DivPerfil, PersonaInformation, Title } from "./styled-profile";
+import { Container } from "./styled-profile";
+import EditIcon from '@mui/icons-material/Edit';
+import { TextNew, TextI } from "./styled-profile";
 
+
+const Profile = () => {
+    const navigate = useNavigate()
+    // const [user, setUser] = useState({})
+    const [ordersHistory, setOrdersHistory] = useState([])
+
+    const [userData] = useRequestData([], `${baseURL}/profile`)
+    const [history] = useRequestData([], `${baseURL}/orders/history`)
+    console.log(userData)
+    console.log(history)
+    const changeProfile = () => {
+        goToEditProfile(navigate)
+
+    }
+
+    const changeAdress = () => {
+        goToAddress(navigate)
+
+    }
+
+    // const orders = history && history.map((order) => {
+    //    return(
+    //        <p>{order.totalPrice}</p>
+
+    //     )
+    // })
+
+
+return (
+   <Container>
+       <Title>Meu perfil</Title>
+    
+    <div>
+   {userData.user ?
+  <TextNew>
+    <p> {userData.user.name}</p>
+     <p>{userData.user.email}</p>
+    <p> {userData.user.cpf}</p>
+     </TextNew>
+: <p>carregando </p> }
+    <div>
+    <EditIcon onClick ={changeProfile}/>
+    </div>
+
+    <TextI>
+   <br/> <p> Endereço cadastrado</p>
+    <p>{userData.adress}</p>
+    <EditIcon onClick ={changeProfile}/>
+    </TextI>
+    </div>
+    
+
+</Container>
+    
+
+    
+    
+    
+
+
+)
+
+    }
 export default Profile;
--- labe-food/src/pages/Search/Search.js
+++ labe-food/src/pages/Search/Search.js
@@ -1,19 +1,20 @@
-<<<<<<< HEAD
 import { baseURL } from "../../constants/baseurl";
 import useRequestData from "../../hooks/useRequestData";
 import { useState } from "react";
 import {SearchIconStyled} from './styled-search'
 import Feed from "../../components/Feed/Feed";
-=======
-import Feed from "../../components/Feed/Feed";
-
->>>>>>> 7040ecba44342ecaac83d598668085e07264cb01
+import { goToDetails } from "../../routes/coordinator";
+import { useNavigate, useParams } from "react-router";
 const Search = () => {
+    const navigate = useNavigate()
+  
 
     return (
         <div>
             <Feed
                 isSearch="true"
+                
+                
             />
         </div>
     )
