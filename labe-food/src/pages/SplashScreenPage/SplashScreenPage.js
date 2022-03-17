9a1199a254d69b832de4ca3a8fe2501daec8081f
Gabriel Dias Lopes Martins <gdlmartins@Unknown.gateway>
2022-03-18 00:20:54 +0800

    setSplashScreen  page

---
+++ labe-food/src/pages/SplashScreenPage/SplashScreenPage.js
@@ -0,0 +1,16 @@
+import {MainStyled} from './style-splashScreen'
+import logowhite from '../../assets/images/whitelogo/logowhite.png'
+
+const SplashScreenPage = () => { 
+
+    return (
+        <MainStyled> 
+            <div>
+            <img src={logowhite} />
+            </div>
+
+        </MainStyled>
+     )
+}
+
+export default SplashScreenPage; 
