8bfcc71407b3e84c31750dd25d70d547fad4b7e0
Gabriel Dias Lopes Martins <gdlmartins@Gabriels-MBP.gateway>
2022-03-18 18:19:31 +0800

    splash Screen animation

--- labe-food/src/pages/SplashScreenPage/style-splashScreen.js
+++ labe-food/src/pages/SplashScreenPage/style-splashScreen.js
@@ -11,16 +11,25 @@ export const MainStyled = styled.div`
     flex-direction: column;
     height: 100vh;
     width: 100%;
-    align-items:center;
+    align-items: center;
     justify-content: center;
     /* padding: 18rem auto; */
   }
-  
+
   img {
-    
     margin: 3rem auto;
     width: 8.075rem;
     height: 5.063rem;
     object-fit: contain;
+    animation: move 3s  ease-in-out;
+  }
+  @keyframes move {
+    0% {
+      transform: scale(0.2) rotate(180deg);
+    }
+    100% {
+      transform: scale(1.0) rotate(720deg);
+    }
+  
   }
 `;
